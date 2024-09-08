0626601de23c78a8d6920a8a4080e498d61828c3
BugSunFish <cod5690@gmail.com>
2024-09-08 20:58:30 +0300

    Initial Commit

---
+++ amogus.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <Windows.h>
+#pragma comment(lib, "Winmm.lib")
+
+int main() {
+    std::cout << ">when alien is sus" << std::endl;
+    std::cout << ">" << std::endl;
+    std::cout << "................" << std::endl;
+    std::cout << "...AMONGUS......" << std::endl;
+    std::cout << ".UNA.....N......" << std::endl;
+    std::cout << ".SGO.....N......" << std::endl;
+    std::cout << ".MOGUSAMON......" << std::endl;
+    std::cout << "...G.....U......" << std::endl;
+    std::cout << "...SA....MO....." << std::endl;
+
+    PlaySound(L"sound.wav", NULL, SND_FILENAME | SND_SYNC | SND_LOOP);
+
+    return 0;
+}
